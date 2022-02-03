7e7572ec15bd1ab6d5efe3a049e26093015ac915
Georg Kaltenbrunner <kaltenbrunnergeorg@googlemail.com>
2022-02-03 13:38:48 +0100

    Main_order created

---
+++ Order/main_order.py
@@ -0,0 +1,32 @@
+import random
+from datetime import datetime, timedelta
+import DueDate
+import ProductionTime
+import MachineNeeded
+import Capacity
+
+d = {}
+class Order:
+
+
+    def __init__(self, date_start, date_end, date_format, production_time_start, production_time_end, max_number_machines, capa_start, capa_end, max_number):
+        self.date_start = date_start
+        self.date_end = date_end
+        self.date_format = date_format
+        self.production_time_start = production_time_start
+        self.production_time_end = production_time_end
+        self.max_number_machines = max_number_machines
+        self. capa_start = capa_start
+        self.capa_end = capa_end
+        self.max_number = max_number
+
+
+
+    def generate_order(self):
+        for i in range(self.max_number):
+            d[i] = MachineNeeded.machine_needed(self.max_number_machines, self.capa_start, self.capa_end)
+        print(d)
+
+order1 = Order("2021/02/01", "2022/03/01", "%Y/%m/%d", 1, 100, 3, 1, 100, 20)
+
+order1.generate_order()
